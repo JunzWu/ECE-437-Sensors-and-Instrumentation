84c987e9c53bcc5ce5adaade7e901ec1b91ba073
JunzWu <46737744+JunzWu@users.noreply.github.com>
2021-08-14 14:04:27 -0500

    Add files via upload

---
+++ 5/lab5_python.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+
+#%%
+# import various libraries necessery to run your Python code
+import time   # time related library
+import sys    # system related library
+ok_loc = 'C:\\Program Files\\Opal Kelly\\FrontPanelUSB\\API\\Python\\3.6\\x64'
+sys.path.append(ok_loc)   # add the path of the OK library
+import ok     # OpalKelly library
+
+#%% 
+# Define FrontPanel device variable, open USB communication and
+# load the bit file in the FPGA
+dev = ok.okCFrontPanel()  # define a device for FrontPanel communication
+SerialStatus=dev.OpenBySerial("")      # open USB communicaiton with the OK board
+ConfigStatus=dev.ConfigureFPGA("U:\ece437\Lab_5_2\Lab_5_2.runs\impl_1\I2C_Transmit.bit"); # Configure the FPGA with this bit file
+
+# Check if FrontPanel is initialized correctly and if the bit file is loaded.
+# Otherwise terminate the program
+print("----------------------------------------------------")
+if SerialStatus == 0:
+    print ("FrontPanel host interface was successfully initialized.")
+else:    
+    print ("FrontPanel host interface not detected. The error code number is:" + str(int(SerialStatus)))
+    print("Exiting the program.")
+    sys.exit ()
+
+if ConfigStatus == 0:
+    print ("Your bit file is successfully loaded in the FPGA.")
+else:
+    print ("Your bit file did not load. The error code number is:" + str(int(ConfigStatus)))
+    print ("Exiting the progam.")
+    sys.exit ()
+print("----------------------------------------------------")
+print("----------------------------------------------------")
+
+
+# First recieve data from the FPGA by using UpdateWireOuts
+while(1):
+    dev.UpdateWireOuts()
+    result1 = dev.GetWireOutValue(0x20)  
+    result2 = dev.GetWireOutValue(0x21)  
+    result=result1*2+result2/128
+    print(result)
+    time.sleep(0.1)
+
+dev.Close
+    
+#%%
